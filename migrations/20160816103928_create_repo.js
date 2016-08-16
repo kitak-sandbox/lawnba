5a1f02a9b1dbddf073434383ae84290c6860b3fb
Keisuke KITA <kei.kita2501@gmail.com>
2016-08-16 16:05:43 +0900

    Resolve ESLint error

--- migrations/20160816103928_create_repo.js
+++ migrations/20160816103928_create_repo.js
@@ -1,18 +1,18 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.hasTable('users').then((exists) => {
-    if (!exists) {
-      return knex.schema.createTable('repos', (t) => {
-        t.increments('id').primary();
-        t.string('name').notNullable().unique();
-        t.string('cron_pattern').notNullable();
-        t.string('base_branch').notNullable();
-
-        t.timestamps();
-      });
-    } else {
-      return new Error("The table already exists");
+    if (exists) {
+      return new Error('The table already exists');
     }
+
+    return knex.schema.createTable('repos', (t) => {
+      t.increments('id').primary();
+      t.string('name').notNullable().unique();
+      t.string('cron_pattern').notNullable();
+      t.string('base_branch').notNullable();
+
+      t.timestamps();
+    });
   });
 };
 
