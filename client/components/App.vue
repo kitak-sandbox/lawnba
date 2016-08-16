ef0ca8c0abdfbdbe6b48548200a39fe9ec96a378
Keisuke KITA <kei.kita2501@gmail.com>
2016-08-16 23:45:40 +0900

    Markup

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,15 +1,39 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <Nav></Nav>
+    <section class="section">
+      <div class="container">
+        <template v-for="i in [1, 2, 3]">
+        <div class="box">
+          <div class="level">
+            <div class="level-left">
+              <p class="title is-4">npm/npm</p>
+            </div>
+            <div class="level-right">
+              <p class="level-item">
+                <a href="#" class="button is-medium is-primary">ジョブを実行</a>
+              </p>
+              <p class="level-item">
+                <a href="#" class="button is-medium">編集</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        </template>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import Nav from './Nav.vue'
+
 export default {
   data () {
-    return {
-      msg: 'Hello Vue 2.0'
-    }
+    return {};
+  },
+  components: {
+    Nav
   }
 }
 </script>
---
+++ client/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="hero is-primary">
+    <nav class="nav">
+      <div class="nav-left">
+        <p class="title nav-item">
+          Lawnba
+        </p>
+      </div>
+      <div class="nav-right nav-menu">
+        <span class="nav-item">
+          <a href="#" class="button is-inverted is-medium is-primary">
+            <span>リポジトリを追加</span>
+          </a>
+        </span>
+      </div>
+    </nav>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
